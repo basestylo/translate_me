787cb71cfcf1d72b20726e59b11d93e058af0c4c
David Martin Garcia <base.dks@gmail.com>
2016-01-20 10:29:19 +0100

    stable version

--- lib/translate_me.rb
+++ lib/translate_me.rb
@@ -2,34 +2,33 @@ require "translate_me/version"
 require 'typhoeus'
 require 'nokogiri'
 require 'uri'
+require 'cgi'
 
 module TranslateMe
   def self.translate word, to, from=""
-    url = parse_url("http://www.google.es/async/translate?")
-    body = "async=translate,sl:#{from},tl:#{to},st:#{parse_text(word)},id:1452609152756,_id:tw-async-translate,_pms:s"
+    url = "http://www.google.es/async/translate?"
+    body = URI.encode("async=translate,sl:#{from},tl:#{to},st:#{word},id:1452609152756,_id:tw-async-translate,_pms:s")
 
     google_response = Typhoeus.post(url, body: body)
     begin
-      html = Nokogiri::HTML(JSON.parse(google_response.body).last.last)
+      html = Nokogiri::HTML(JSON.parse(google_response.body.to_s)[1][1])
       response = {
-        word: html.css("span")[3].children.text,
+        word: html.css("span")[3].children.text.force_encoding("ISO-8859-1").encode("utf-8"),
         synonyms:  html.css(".tw-bilingual-translation").map{|e| e.text},
         source_language: html.css("span")[5].text
       }
     rescue JSON::ParserError => e
       puts e.message
       puts e.backtrace
-      puts "URL: " + url
-      puts "Body: " + body
+      puts "URL: " + url.to_s
+      puts "Body: " + body.to_s
+      puts "Word: " + word.to_s
     end
     return response || nil
   end
 
-  def self.parse_url url
-    URI.parse(url)
-  end
-
   def self.parse_text text
-    text.downcase.gsub(",","%2C").gsub("?","%3F")
+    puts "GEMA: " + text
+    CGI.escape(text)
   end
 end
--- lib/translate_me/CHANGELOG.md
+++ lib/translate_me/CHANGELOG.md
@@ -1,5 +1,7 @@
 - V0.1.0 Google translate API working
 
 - V0.1.1 Source Language detection
- 
+
 - v0.1.2 up version after yank
+
+- v0.1.4 undesired downcase, implicit casting error, parsing specian chars from google
